9e5d4ef734475d3fbd925bd5e92606d38123cad3
KarlRom <32362720+KarlRom@users.noreply.github.com>
2025-02-17 08:33:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 from use_cases import AccountService
+from infrastructure import AccountRepository
+from domain import Account
 
 def main():
-    service = AccountService()
+    repo = AccountRepository()  # Create repository
+    service = AccountService(repo)  # Pass repository to service
 
     #Creates new account
-    account = service.create_account(1, "John Doe", "john@example.com", "1234567890", "123-456-789")
+    account = service.create_account(101, "John Doe", "john@example.com", "1234567890", "123-456-789")
     print(f"Created account: {account.account_number} with balance {account.get_balance()}")
 
     #Make transactions
     service.make_transaction(account.account_id, 500, 'deposit')
-    print(service.generate_account_statement(account.account_id))
-
     service.make_transaction(account.account_id, 200, 'withdraw')
-    print(service.generate_account_statement(account.account_id))
+
+    statement = service.generate_account_statement(account.account_id)
+    print("\n" + statement)
 
 if __name__ == "__main__":
     main()
